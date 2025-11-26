ef028a8cceaa855a23446139963f047cae874878
Raghav Tandon <tanraag@gmail.com>
2025-11-26 12:11:14 -0500

    setup: SQLAlchemy database module; create tables on startup

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import Base, engine, SessionLocal
 
 app = FastAPI()
+
+# create tables on startup
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def health():
